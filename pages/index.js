eb6dd7a7b3a7e04d3060b66c53dcd3c71c3034a8
Emil Lipskij <emil.lipskij@gmail.com>
2021-03-26 18:04:15 +0200

    Add simple logo

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Footer from "../components/Footer";
+import Banner from "../components/Banner";
 
 export default function Home() {
   return (
@@ -13,31 +14,43 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Image src='/easylogo.png' alt='logo' width={200} height={100} />
+        <Banner />
         <div className={styles.navigationCard}>
-          <a className={styles.card} href='/art'>
-            C
-          </a>
+          <Link href='/C'>
+            <a className={styles.card} href='/art'>
+              C
+            </a>
+          </Link>
           <Link href='/art'>
             <a className={styles.card} href='/art'>
               Art
             </a>
           </Link>
-          <a className={styles.card} href='/art'>
-            Cultural Events
-          </a>
-          <a className={styles.card} href='/art'>
-            P
-          </a>
-          <a className={styles.card} href='/art'>
-            M
-          </a>
-          <a className={styles.card} href='/art'>
-            D
-          </a>
-          <a className={styles.card} href='/art'>
-            ¯\_(ツ)_/¯
-          </a>
+          <Link href='/culturalEvents'>
+            <a className={styles.card} href='/art'>
+              Cultural Events
+            </a>
+          </Link>
+          <Link href='/P'>
+            <a className={styles.card} href='/art'>
+              P
+            </a>
+          </Link>
+          <Link href='/M'>
+            <a className={styles.card} href='/art'>
+              M
+            </a>
+          </Link>
+          <Link href='/D'>
+            <a className={styles.card} href='/art'>
+              D
+            </a>
+          </Link>
+          <Link href='idk'>
+            <a className={styles.card} href='/art'>
+              ¯\_(ツ)_/¯
+            </a>
+          </Link>
           <Link href='/contacts'>
             <a className={styles.card}>Contacts</a>
           </Link>
