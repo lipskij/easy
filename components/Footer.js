43b41d6c3a7dc62e6e629c5a10ca25c83e1797c8
Emil Lipskij <emil.lipskij@gmail.com>
2021-03-27 10:58:59 +0200

    Call to action backround color

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,35 @@
+import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 
 const Footer = () => {
   return (
     <div className='footer'>
-      <p>All rights reserved by <i>easy.com</i></p>
+      <div className='footer-logos'>
+        <Link href='youtube.com'>
+          <a target='_blank'>
+            <Image src='/youtube.png' alt='yt-logo' width={25} height={25} />
+          </a>
+        </Link>
+        <Link href='youtube.com'>
+          <a target='_blank'>
+            <Image src='/facebook.png' alt='yt-logo' width={25} height={25} />
+          </a>
+        </Link>
+        <Link href='youtube.com'>
+          <a target='_blank'>
+            <Image src='/instagram.png' alt='yt-logo' width={25} height={25} />
+          </a>
+        </Link>
+        <Link href='youtube.com'>
+          <a target='_blank'>
+            <Image src='/vimeo.png' alt='yt-logo' width={25} height={25} />
+          </a>
+        </Link>
+      </div>
+      <p>
+        All rights reserved by <i>easy.com</i>
+      </p>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -14,48 +14,66 @@ export default function Home() {
 
       <main className={styles.main}>
         <Banner />
+        <div className={styles.homeText}>
+          <h1>Mūsų veikla</h1>
+        </div>
         <div className={styles.navigationCard}>
-          <Link href='/commercial'>
-            <a className={styles.card} style={{ backgroundColor: "#ede6a8" }}>
-              Commercial
-            </a>
-          </Link>
-          <Link href='/art'>
-            <a className={styles.card} style={{ backgroundColor: "#eda8a8" }}>
-              Art
-            </a>
-          </Link>
-          <Link href='/culturalEvents'>
-            <a className={styles.card} style={{ backgroundColor: "#a8afed" }}>
-              Cultural Events
-            </a>
-          </Link>
-          <Link href='/p'>
-            <a className={styles.card} style={{ backgroundColor: "#d0eda8" }}>
-              Private
-            </a>
-          </Link>
-
-          <Link href='/m'>
-            <a className={styles.card} style={{ backgroundColor: "#a8edd3", marginLeft: '14rem' }}>
-              Mokymai
-            </a>
-          </Link>
-          <Link href='/d'>
-            <a className={styles.card} style={{ backgroundColor: "#a8d4ed" ,marginLeft: '14rem'}}>
-              Dirbtuves
-            </a>
-          </Link>
-          {/* <Link href='/idk'>
-            <a className={styles.card} style={{ backgroundColor: "#bfa8ed" }}>
-              ¯\_(ツ)_/¯
-            </a>
-          </Link> */}
-          <Link href='/contacts'>
-            <a className={styles.card} style={{ backgroundColor: "#eda8d5",marginLeft: '14rem' }}>
-              Contacts
-            </a>
-          </Link>
+          <div className={styles.navIndividualCard}>
+            <Link href='/commercial'>
+              <a className={styles.card}>Komercija</a>
+            </Link>
+            <p>Komerciniai renginiai</p>
+            <p>ir projektai.</p>
+          </div>
+          <div className={styles.navIndividualCard}>
+            <Link href='/art'>
+              <a className={styles.card}>Art</a>
+            </Link>
+            <p>Meniniai projektai</p>
+            <p>ir ju ispildymas</p>
+          </div>
+          <div className={styles.navIndividualCard}>
+            <Link href='/culturalEvents'>
+              <a className={styles.card}>Kultūriniai renginiai</a>
+            </Link>
+            <p>Kulturiniai renginiai</p>
+            <p>Miesto sventes.</p>
+          </div>
+          <div className={styles.navIndividualCard}>
+            <Link href='/p'>
+              <a className={styles.card}>Private</a>
+            </Link>
+            <p>Privatiems klientams</p>
+            <p>Uzsakymai it kita</p>
+            <p>informacija.</p>
+          </div>
+          <div className={styles.navIndividualCard}>
+            <Link href='/m'>
+              <a className={styles.card}>Mokymai</a>
+            </Link>
+            <p>Kursai, seminarai,</p>
+            <p>paskaitos, apmokymai.</p>
+          </div>
+          <div className={styles.navIndividualCard}>
+            <Link href='/d'>
+              <a className={styles.card}>Dirbtuvės</a>
+            </Link>
+            <p>Pazintinai mokymai</p>
+            <p>'work to grow'.</p>
+          </div>
+          <div className={styles.navIndividualCard}>
+            <Link href='/idk'>
+              <a className={styles.card}>¯\_(ツ)_/¯</a>
+            </Link>
+            <p>As nezinau ar busiu cia</p>
+            <p>nes neiskaiciau failo.</p>
+          </div>
+          <div className={styles.navIndividualCard}>
+            <Link href='/contacts'>
+              <a className={styles.card}>Kontaktai</a>
+            </Link>
+            <p>Susisiekite su mumis!</p>
+          </div>
         </div>
       </main>
       <Footer />
