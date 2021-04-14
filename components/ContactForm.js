37fd485ae0c4a0bf4e18e9bb3305e9d6c3b9360f
Emil Lipskij <emil.lipskij@gmail.com>
2021-04-14 19:34:48 +0300

    Add form err messages

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/Contacts.module.css";
 
 export default function ContactForm() {
+  const [text, setText] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const nameInput = (values) => {
+    if (values.target.value.length > 25) {
+      setText("Name is too long");
+    } else {
+      setText("");
+    }
+  };
+
+  const emailInput = (email) => {
+    console.log(email.target.value);
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!re.test(email.target.value)) {
+      setEmail("Invalid email adress");
+      return re.test(email);
+    } else {
+      setEmail("");
+    }
+  };
+
+  const messageInput = (message) => {
+    if (message.target.value.length > 200) {
+      setMessage("Message is too long");
+    } else {
+      setMessage("");
+    }
+  };
   return (
     <div className={styles.emailCell}>
       <div className={styles.emailForm}>
@@ -11,7 +41,9 @@ export default function ContactForm() {
             <label htmlFor='name' className='sr-only'>
               Vardas
             </label>
+            <p>{text}</p>
             <input
+              onChange={nameInput}
               type='text'
               name='name'
               className={styles.name}
@@ -22,7 +54,9 @@ export default function ContactForm() {
             <label htmlFor='email' className='sr-only'>
               Email
             </label>
+            <p>{email}</p>
             <input
+              onChange={emailInput}
               name='email'
               type='text'
               className={styles.name}
@@ -33,7 +67,9 @@ export default function ContactForm() {
             <label htmlFor='message' className='sr-only'>
               Message
             </label>
+            <p>{message}</p>
             <textarea
+              onChange={messageInput}
               name='message'
               rows='4'
               className={styles.subject}
